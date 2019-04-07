5985bb7b1981e33f0776251044b2bd22c75dc53d
Lev Saltsyn <lev.santsyn@gmail.com>
2019-04-07 11:39:19 +0300

    add mobile nav

--- public/js/app.js
+++ public/js/app.js
@@ -29,7 +29,7 @@ $(document).ready(function() {
 
     $("#respMenu").aceResponsiveMenu({
         resizeWidth: '768', // Set the same in Media query       
-        animationSpeed: 'fast', //slow, medium, fast
+        animationSpeed: 'medium', //slow, medium, fast
         accoridonExpAll: false   //Expands all the accordion menu on click
     });
 });
